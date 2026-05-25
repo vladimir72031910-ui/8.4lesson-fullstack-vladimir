d15448b52f1e24f52012f4ff582746c2e79eb5ce
vova <vladimir72031910@gmail.com>
2026-05-25 17:45:06 +0900

    feat: ipad-vers

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ const swiper = new Swiper('.brands', {
    }
 
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.querySelector('.show-hidden');
+    const arrow = document.querySelector('.expand');
+    const textSpan = document.querySelector('.hiddens');
+
+button.addEventListener('click', function() {
+    const isHidden = document.querySelector('.hidden-brands').classList.toggle('hidden');
+    if (isHidden) {
+        textSpan.textContent = 'Показать все';
+        arrow.style.transform = 'rotate(0deg)';
+
+    }  else {
+        textSpan.textContent = 'Скрыть';
+        arrow.style.transform = 'rotate(180deg)';
+    }
+   
+});
+
+});
